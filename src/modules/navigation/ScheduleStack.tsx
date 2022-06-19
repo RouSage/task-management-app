6efeeeaae89cee20eb6e3f4107832c70f51c26d5
RouSage <kolbasovsergey02@gmail.com>
2022-06-19 17:19:13 +0300

    feat: style schedule header

--- src/modules/navigation/ScheduleStack.tsx
+++ src/modules/navigation/ScheduleStack.tsx
@@ -1,5 +1,6 @@
 import ScheduleScreen from '@modules/schedule/ScheduleScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import theme from '@theme';
 
 import { RootStackParamList, Routes } from './types';
 
@@ -7,7 +8,19 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const ScheduleStack = () => (
   <Stack.Navigator>
-    <Stack.Screen name={Routes.Schedule} component={ScheduleScreen} />
+    <Stack.Screen
+      name={Routes.Schedule}
+      component={ScheduleScreen}
+      options={{
+        headerShadowVisible: false,
+        headerTitleStyle: {
+          color: theme.colors.black['50'],
+          fontFamily: 'Poppins-Bold',
+          fontSize: 18,
+          fontWeight: 'bold',
+        },
+      }}
+    />
   </Stack.Navigator>
 );
 
